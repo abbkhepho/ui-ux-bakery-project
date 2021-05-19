39cd8c96d2d01e1ace4c32e23a6754aad47c6f1f
abbkhepho <43946998+abbkhepho@users.noreply.github.com>
2021-05-19 13:09:45 +0200

    last

---
+++ src/components/CustomCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <img :src="image_url" :alt="title">
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'CustomCards',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        image_url: {
+            type: String,
+            default: ''
+        },
+        description: {
+            type: String,
+            default: ''
+        },
+        price: {
+            type: Number,
+            default: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="">
-
+  <div class="hej">
+    <h1>HEJ</h1>
+    <!-- <custom-card title="Lemon cheesecake" image_url="https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/lemoncheese_cake.jpg" description="abc" price="4"/> -->
   </div>
 </template>
 
 <script>
+import CustomCard from "../components/CustomCard"
+
 export default {
   name: 'PageIndex',
+  components: {
+    CustomCard: "custom-card"
+  },
   data(){
     return {
       loggedIn: false
