44b0fa9a23d1f956a35eea9d4eddebf037ae56b6
Saika0k1 <43946998+abbkhepho@users.noreply.github.com>
2021-05-25 10:01:31 +0200

    gogo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,14 @@
 <template>
-<<<<<<< Updated upstream
-  <div class="row q-ma-sm q-gutter-sm justify-center">
-    <!-- Creates a CustomCard for every cake !-->
-    <div v-for="cake in cakes" :key="cake.title">
-      <CustomCard
-=======
   <div class="row justify-center">
     <div class="row col-8 q-ma-sm q-mt-none q-gutter-sm justify-center">
       <div v-for="cake in cakes" :key="cake.title">
           <CakeCard
->>>>>>> Stashed changes
             :title="cake.title"
             :imageurl="cake.image"
             :description="cake.detailDescription"
             :price="cake.price"
-<<<<<<< Updated upstream
-      />
-=======
           />
       </div>
->>>>>>> Stashed changes
     </div>
   </div>
 </template>
