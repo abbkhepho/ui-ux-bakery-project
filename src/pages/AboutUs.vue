c87f3b3a795470fb8555debcaf5884c4cd34f601
Saika0k1 <43946998+abbkhepho@users.noreply.github.com>
2021-05-25 09:03:59 +0200

    added about us

---
+++ src/pages/AboutUs.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row justify-between">
+    <div class="row col-7 q-ma-sm q-mt-none q-gutter-sm">
+        <div>
+            <h2 class="q-ma-none">About us</h2>
+        </div>
+        <div>
+            <p>
+                Over at Bear island bakery we have a simple and down to earth mindset, be who you want to be, that’s why we at Bear island bakery have chosen to remain local and loyal to our customers. We have chosen to remain unchanged and keep our pasteries locally produced while also helping other local businesses by buying their ingredients and equipment. Envoirment is another important core value hear at Bear island bakery, treat as you want to be treated, by using envoirment friendly transportation measures we are reducing the pressure on mother earth while staying true to ourselves. Come on by and try our envoirment friendly, locally produced and honest pastries.
+            </p>
+        </div>
+    </div>
+    <div class="row col-4">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+p {
+    font-size: 18px;
+}
+</style>
